0dfb1f6e5b83b55f1d69c9628efd0053ea890e93
James Mwendwa <mwendwajames32@gmail.com>
2022-04-27 11:54:40 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Counter } from './features/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h2>hello</h2>
+    </div>
+  );
+}
+
+export default App;
